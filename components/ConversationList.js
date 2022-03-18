e0c5bcb72e75732894afde9266dfb505b1cf10c4
AayushMohan <aayushmohan1702@gmail.com>
2022-03-18 16:50:52 +0530

    Added Dummy Dms, useState, conversations container, elements container, svg container

--- components/ConversationList.js
+++ components/ConversationList.js
@@ -3,12 +3,70 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import friends from "../assets/icons/friends.svg";
 import nitro from "../assets/icons/nitro.svg";
+import avatar1 from "../assets/avatar-1.webp";
+import avatar2 from "../assets/avatar-2.png";
+import avatar3 from "../assets/avatar-3.webp";
+import avatar4 from "../assets/avatar-4.webp";
+
+const dummyDms = [
+  {
+    id: 1,
+    name: "Aayush",
+    avatar: avatar1,
+  },
+  {
+    id: 2,
+    name: "David",
+    avatar: avatar2,
+  },
+  {
+    id: 3,
+    name: "Amaanath",
+    avatar: avatar3,
+  },
+  {
+    id: 4,
+    name: "Frankie",
+    avatar: avatar4,
+  },
+];
 
 const ConversationList = () => {
+  const [dms, setDms] = useState(dummyDms);
   return (
     <div className={styles.conversations}>
       <div className={styles.conversationListTop}>
         <input type="search" placeholder="Find or start a conversation" />
+      </div>
+      <div className={styles.conversationsContainer}>
+        <div className={styles.elementsContainer}>
+          <div className={styles.svgContainer}>
+            <Image
+              height={25}
+              width={25}
+              src={friends}
+              className={styles.svg}
+              alt="friends"
+            />
+          </div>
+          <p>Friends</p>
+        </div>
+        <div className={styles.elementsContainer}>
+          <div className={styles.svgContainer}>
+            <Image
+              height={25}
+              width={25}
+              src={nitro}
+              className={styles.svg}
+              alt="nitro"
+            />
+          </div>
+          <p>Nitro</p>
+        </div>
+        <div className={styles.dmTitle}>DIRECT MESSAGES</div>
+        {dms.map((dm, index) => (
+          <div>dm card</div>
+        ))}
       </div>
     </div>
   );
