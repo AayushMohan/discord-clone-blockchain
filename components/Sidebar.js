528e3013d62d58dc1e767d8b737381f0e75d0298
AayushMohan <aayushmohan1702@gmail.com>
2022-03-18 15:09:55 +0530

    Added Avatars, Icons, dummyChannels, router & useState

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,7 +2,38 @@ import styles from "../styles/sidebar.module.css";
 import { useEffects, useState } from "react";
 import { useRouter } from "next/router";
 
+import avatar1 from "../assets/avatar-1.webp";
+import avatar2 from "../assets/avatar-2.png";
+import avatar3 from "../assets/avatar-3.webp";
+import avatar4 from "../assets/avatar-4.webp";
+
+const dummyChannels = [
+  {
+    id: 1,
+    name: "general",
+    avatar: avatar1,
+  },
+  {
+    id: 2,
+    name: "random",
+    avatar: avatar2,
+  },
+  {
+    id: 3,
+    name: "chill",
+    avatar: avatar3,
+  },
+  {
+    id: 4,
+    name: "buildspace",
+    avatar: avatar4,
+  },
+];
+
 const Sidebar = () => {
+  const router = useRouter();
+  const [channels, setChannels] = useState(dummyChannels);
+
   return <div className={styles.wrapper}>Sidebar</div>;
 };
 
