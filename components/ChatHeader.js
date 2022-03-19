2a89767be6e6eacd86fbc4cc85b84566c6b40835
AayushMohan <aayushmohan1702@gmail.com>
2022-03-19 18:43:18 +0530

    Added Chat Header Icons

--- components/ChatHeader.js
+++ components/ChatHeader.js
@@ -22,16 +22,68 @@ const ChatHeader = () => {
         <div className={styles.chatHeaderStatus} id="online" />
       </div>
       {currentAccount ? (
-        <div className={styles.connectWallet}>
+        <div className={styles.connectedWallet}>
           <Image src={ethLogo} height={20} width={15} alt="ethLogo" />
           <span className={styles.separator}>{"|"}</span>
           {currentAccount.slice(0, 6)}...{currentAccount.slice(39)}
         </div>
       ) : (
-        <div className={styles.connectWallet} onClick={() => connectWallet()}>
-          Connect Wallet
-        </div>
+        <div className={styles.connectWallet}>Connect Wallet</div>
       )}
+
+      <div className={styles.headerIconsContainer}>
+        <div className={styles.headerItems}>
+          <Image
+            height={25}
+            width={25}
+            src={phone}
+            className={styles.svg}
+            alt=""
+          />
+        </div>
+        <div className={styles.headerItems}>
+          <Image
+            height={25}
+            width={25}
+            src={video}
+            className={styles.svg}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className={styles.headerItem}>
+        <Image height={25} width={25} src={pin} className={styles.svg} alt="" />
+      </div>
+      <div className={styles.headerItem}>
+        <Image
+          height={25}
+          width={25}
+          src={personPlus}
+          className={styles.svg}
+          alt=""
+        />
+      </div>
+      <div className={styles.headerItem}>
+        <input type="search" placeholder="Search" />
+      </div>
+      <div className={styles.headerItem}>
+        <Image
+          height={25}
+          width={25}
+          src={inbox}
+          className={styles.svg}
+          alt=""
+        />
+      </div>
+      <div className={styles.headerItem}>
+        <Image
+          height={25}
+          width={25}
+          src={helpers}
+          className={styles.svg}
+          alt=""
+        />
+      </div>
     </div>
   );
 };
