d8f2c399d5cd91e886af4de379561493403e96de
AayushMohan <aayushmohan1702@gmail.com>
2022-03-21 21:19:56 +0530

    Added Reducer

--- context/context.js
+++ context/context.js
@@ -4,4 +4,14 @@ import Gun from "gun";
 
 export const DiscordContext = createContext();
 
-// const gun = Gun(["https://discord])
+const gun = Gun(["https://discord-gun-node.herokuapp.com/"]);
+
+const reducer = (state, action) => {
+  try {
+    if (action.type == "clear") return { messages: [] };
+    if (action.type == "add")
+      return { messages: [...state.messages, action.data] };
+  } catch (error) {
+    console.error(error);
+  }
+};
